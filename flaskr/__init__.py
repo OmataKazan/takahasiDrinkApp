2df9b6d561fc0a9fbd85ede5967c87509d215224
OmataKazan <42262680+OmataKazan@users.noreply.github.com>
2020-11-04 17:17:11 +0900

    セットアップを完了

---
+++ flaskr/__init__.py
@@ -0,0 +1,18 @@
+#__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+db = SQLAlchemy()
+migrate = Migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'aefacregadfaerfrgl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    from flaskr.views import bp
+    app.register_blueprint(bp)
+    return app
