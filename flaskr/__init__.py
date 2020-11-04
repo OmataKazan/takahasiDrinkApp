90668d3d445f5b61fbbad8afe2f4e3a8d89dc38d
OmataKazan <42262680+OmataKazan@users.noreply.github.com>
2020-11-04 23:04:45 +0900

    ブラウザからの登録〜表示まで

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,13 +6,17 @@ from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 db = SQLAlchemy()
-migrate = Migrate
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'aefacregadfaerfrgl'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from flaskr.views import bp
+    app.register_blueprint(bp)
     from flaskr.views import bp
     app.register_blueprint(bp)
     return app
